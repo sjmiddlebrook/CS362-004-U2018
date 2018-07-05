b48a1fd13fab5930c43cd5d9593168a355bae900
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-07-05 18:45:55 +0200

    refactor smithy card

--- projects/middlebs/dominion/dominion.c
+++ projects/middlebs/dominion/dominion.c
@@ -737,14 +737,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
             return 0;
 
         case smithy:
-            //+3 Cards
-            for (i = 0; i < 3; i++) {
-                drawCard(currentPlayer, state);
-            }
-
-            //discard card from hand
-            discardCard(handPos, currentPlayer, state, 0);
-            return 0;
+            return smithyCard(currentPlayer, handPos, state);
 
         case village:
             //+1 Card
@@ -1199,6 +1192,18 @@ int adventurerCard(int currentPlayer, struct gameState *state) {
                                                                                        1]; // discard all cards in play that have been drawn
         z = z - 1;
     }
+    return 0;
+}
+
+int smithyCard(int currentPlayer, int handPos, struct gameState *state) {
+    int i;
+    //+3 Cards
+    for (i = 0; i < 3; i++) {
+        drawCard(currentPlayer, state);
+    }
+
+    //discard card from hand
+    discardCard(handPos, currentPlayer, state, 0);
     return 0;
 }
 
--- projects/middlebs/dominion/dominion.h
+++ projects/middlebs/dominion/dominion.h
@@ -128,6 +128,8 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int adventurerCard();
+int adventurerCard(int player, struct gameState *state);
+
+int smithyCard(int player, int handPos, struct gameState *state);
 
 #endif
