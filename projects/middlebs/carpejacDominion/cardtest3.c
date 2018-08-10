7f9ee25147c9bcd489a026f1cf93bb3f4ea7e959
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-08-10 15:40:52 +0200

    add card tests and update makefile

---
+++ projects/middlebs/carpejacDominion/cardtest3.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+void assertTrue(int a, int b) {
+    if (a == b) {
+        printf("\tPassed Test\n\n");
+    } else {
+        printf("\tFailed Test\n\n");
+    }
+}
+
+
+void testVillageCard() {
+    printf("TESTING Village Card\n");
+    int kingdomCards[10] = {council_room, mine, remodel, smithy, village, adventurer, sea_hag, minion, cutpurse, gardens};
+    struct gameState currentGameState, beginningGameState;
+
+    initializeGame(2, kingdomCards, 5, &currentGameState);
+    memcpy(&beginningGameState, &currentGameState, sizeof(struct gameState));
+
+    int bonus = 0;
+    int villageCardEffect = cardEffect(village, 0, 0, 0, &currentGameState, 0, &bonus);
+    printf("\tTest if village card effect returns 0:\n");
+    assertTrue(villageCardEffect, 0);
+
+    printf("\tTest deck count decreases by 1 after village is played:\n");
+    assertTrue(currentGameState.deckCount[0], beginningGameState.deckCount[0]-1);
+
+    printf("\tTest hand count is the same after village is played:\n");
+    assertTrue(currentGameState.handCount[0], beginningGameState.handCount[0]);
+
+    printf("\tTest number of actions increases by 2 after village is played:\n");
+    assertTrue(currentGameState.numActions, beginningGameState.numActions+2);
+
+    printf("\tTest kingdom cards supply count remains the same:\n");
+    int i;
+    for (i = 0; i < 10; i++) {
+        assertTrue(currentGameState.supplyCount[kingdomCards[i]], beginningGameState.supplyCount[kingdomCards[i]]);
+    }
+
+    printf("\tTest victory cards supply count remains the same:\n");
+    assertTrue(currentGameState.supplyCount[province], beginningGameState.supplyCount[province]);
+    assertTrue(currentGameState.supplyCount[duchy], beginningGameState.supplyCount[duchy]);
+    assertTrue(currentGameState.supplyCount[estate], beginningGameState.supplyCount[estate]);
+
+    printf("\tTest treasure cards supply count remains the same:\n");
+    assertTrue(currentGameState.supplyCount[copper], beginningGameState.supplyCount[copper]);
+    assertTrue(currentGameState.supplyCount[silver], beginningGameState.supplyCount[silver]);
+    assertTrue(currentGameState.supplyCount[gold], beginningGameState.supplyCount[gold]);
+
+    printf("\tTest other player state not changing:\n");
+    assertTrue(currentGameState.deckCount[1], beginningGameState.deckCount[1]);
+    assertTrue(currentGameState.handCount[1], beginningGameState.handCount[1]);
+}
+
+int main(int argc, char *argv[]) {
+    testVillageCard();
+    return 0;
+}
---
+++ projects/middlebs/carpejacDominion/cardtest4.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+void assertTrue(int a, int b) {
+    if (a == b) {
+        printf("\tPassed Test\n\n");
+    } else {
+        printf("\tFailed Test\n\n");
+    }
+}
+
+
+void testCouncilRoomCard() {
+    printf("TESTING Council Room Card\n");
+    int kingdomCards[10] = {council_room, mine, remodel, smithy, village, adventurer, sea_hag, minion, cutpurse, gardens};
+    struct gameState currentGameState, beginningGameState;
+
+    initializeGame(2, kingdomCards, 5, &currentGameState);
+    memcpy(&beginningGameState, &currentGameState, sizeof(struct gameState));
+
+    int bonus = 0;
+    int councilRoomCardEffect = cardEffect(council_room, 0, 0, 0, &currentGameState, 0, &bonus);
+    printf("\tTest if council room card effect returns 0:\n");
+    assertTrue(councilRoomCardEffect, 0);
+
+    printf("\tTest deck count decreases by 4 after council room is played:\n");
+    assertTrue(currentGameState.deckCount[0], beginningGameState.deckCount[0]-4);
+
+    printf("\tTest hand count has 3 more cards after council room is played:\n");
+    assertTrue(currentGameState.handCount[0], beginningGameState.handCount[0]+3);
+
+    printf("\tTest buy increases by 1 after council room is played:\n");
+    assertTrue(currentGameState.numBuys, beginningGameState.numBuys+1);
+
+    printf("\tTest kingdom cards supply count remains the same:\n");
+    int i;
+    for (i = 0; i < 10; i++) {
+        assertTrue(currentGameState.supplyCount[kingdomCards[i]], beginningGameState.supplyCount[kingdomCards[i]]);
+    }
+
+    printf("\tTest victory cards supply count remains the same:\n");
+    assertTrue(currentGameState.supplyCount[province], beginningGameState.supplyCount[province]);
+    assertTrue(currentGameState.supplyCount[duchy], beginningGameState.supplyCount[duchy]);
+    assertTrue(currentGameState.supplyCount[estate], beginningGameState.supplyCount[estate]);
+
+    printf("\tTest treasure cards supply count remains the same:\n");
+    assertTrue(currentGameState.supplyCount[copper], beginningGameState.supplyCount[copper]);
+    assertTrue(currentGameState.supplyCount[silver], beginningGameState.supplyCount[silver]);
+    assertTrue(currentGameState.supplyCount[gold], beginningGameState.supplyCount[gold]);
+
+    printf("\tTest other player hand count increases by 1:\n");
+    assertTrue(currentGameState.handCount[1], beginningGameState.handCount[1]+1);
+
+}
+
+int main(int argc, char *argv[]) {
+    testCouncilRoomCard();
+    return 0;
+}
